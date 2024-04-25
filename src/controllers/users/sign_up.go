b62617f61a206ea48885d8b02a50dd548cf98a6f
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2024-04-25 12:01:36 -0300

    feat: validation request object

--- src/controllers/users/sign_up.go
+++ src/controllers/users/sign_up.go
@@ -1,5 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func SignUp(c *gin.Context) {}
+	rest_err "github.com/Sid5488/go-crud/src/configurations"
+	"github.com/Sid5488/go-crud/src/models/request"
+	"github.com/gin-gonic/gin"
+)
+
+func SignUp(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+	}
+
+	fmt.Println(userRequest)
+}
